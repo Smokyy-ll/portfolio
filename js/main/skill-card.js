a22e7e3d20b35b6457f619f897945881a26fe8f2
Kirill Magonov <magonov09@bk.ru>
2024-03-02 13:32:18 +0300

    feat: added fade for skills-card

--- js/main/skill-card.js
+++ js/main/skill-card.js
@@ -1,12 +1,17 @@
 const skillsSection = document.querySelector('#skills');
+const fadeBlock = document.querySelector('.fade-block');
 
 skillsSection.addEventListener('click', (e) => {
     if (e.target.closest('.button-action')) {
         e.target.closest('.skills-card').classList.add('active');
+        fadeBlock.classList.add('open');
     }
 
     if (e.target.closest('.skills-card__close')) {
         e.target.closest('.skills-card').classList.remove('active');
+        fadeBlock.classList.remove('open');
     }
+
+
 })
 
