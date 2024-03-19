33320b43dd475bce7954cd6a0033444587d9c54a
Kirill Magonov <magonov09@bk.ru>
2024-03-19 19:53:00 +0300

    feat: added animation for block skills

--- js/main/animation.js
+++ js/main/animation.js
@@ -8,3 +8,17 @@ tlStart.from('.info-author', {
     opacity:0,
     ease:'bounce',
 })
+
+gsap.from('.skills-card',{
+    stagger:0.3,
+    opacity:0,
+    y:200,
+    scrollTrigger:{
+        // markers:true,
+        trigger: '.skills',
+        start: 'top 70%',
+        end:'30% 50%',
+        scrub: 2,
+        toggleActions: 'play none none reverse',
+    }
+})
