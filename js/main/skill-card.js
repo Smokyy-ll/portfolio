831269011b11d90ef2fb55616c85ec17162c90b9
Kirill Magonov <magonov09@bk.ru>
2024-03-23 13:45:00 +0300

    fix: fixed skills-card

--- js/main/skill-card.js
+++ js/main/skill-card.js
@@ -5,11 +5,13 @@ skillsSection.addEventListener('click', (e) => {
     if (e.target.closest('.button-action')) {
         e.target.closest('.skills-card').classList.add('active');
         fadeBlock.classList.add('open');
+        document.body.classList.add('active');
     }
 
     if (e.target.closest('.skills-card__close')) {
         e.target.closest('.skills-card').classList.remove('active');
         fadeBlock.classList.remove('open');
+        document.body.classList.remove('active');
     }
 
 
