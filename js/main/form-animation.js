79c30a0387d231c0ceba1d00677a18e3da818dc5
Kirill Magonov <magonov09@bk.ru>
2024-04-20 13:28:43 +0300

    feat: added handler for event blur

--- js/main/form-animation.js
+++ js/main/form-animation.js
@@ -1,9 +1,15 @@
 const inputsForm = document.querySelectorAll(".form-application__input");
 
 inputsForm.forEach(input => {
+    const formItem = input.closest('.form-application__item');
+    const placeholder = formItem.querySelector('.form-application__placeholder');
     input.addEventListener('focus', () => {
-        const formItem = input.closest('.form-application__item');
-        const placeholder = formItem.querySelector('.form-application__placeholder');
         placeholder.classList.add('focus');
     })
-});
+
+    input.addEventListener('blur', () => {
+        if (input.value.trim() < 1) {
+            placeholder.classList.remove('focus');
+        }
+    })
+})
