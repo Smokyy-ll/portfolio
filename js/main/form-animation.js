4550c8f5acc42c7786ce191ea7842ff5a6418102
Kirill Magonov <magonov09@bk.ru>
2024-05-25 13:48:08 +0300

    refactor: changed scripts for animation and validation of form

--- js/main/form-animation.js
+++ js/main/form-animation.js
@@ -1,15 +1,31 @@
-const inputsForm = document.querySelectorAll(".form-application__input");
+const form = document.querySelector('#form');
 
-inputsForm.forEach(input => {
-    const formItem = input.closest('.form-application__item');
-    const placeholder = formItem.querySelector('.form-application__placeholder');
-    input.addEventListener('focus', () => {
-        placeholder.classList.add('focus');
-    })
-
-    input.addEventListener('blur', () => {
-        if (input.value.trim() < 1) {w
-            placeholder.classList.remove('focus');
+const animationPlaceholder = (e) => {
+    const inputElement = e.target.closest('[data-input]');
+    if (inputElement) {
+        const formElement = inputElement.closest('[data-form-element]');
+        const fakePlaceholder = formElement.querySelector('[data-fake-placeholder]');
+        fakePlaceholder.classList.add('focus');
+        if (inputElement.id === 'message') {
+            inputElement.classList.add('focus');
         }
-    })
-})
+        inputElement.addEventListener('blur', removeFocusFromPlaceholder);
+    }
+}
+
+const removeFocusFromPlaceholder = (e) => {
+    const inputElement = e.target.closest('[data-input]');
+    if (inputElement) {
+        const formElement = inputElement.closest('[data-form-element]');
+        const fakePlaceholder = formElement.querySelector('[data-fake-placeholder]');
+        if (inputElement.value.length < 1) {
+            fakePlaceholder.classList.remove('focus');
+            if (inputElement.id === 'message') {
+                inputElement.classList.remove('focus');
+            }
+        }
+        inputElement.removeEventListener('blur', removeFocusFromPlaceholder);
+    }
+}
+
+form.addEventListener('click', animationPlaceholder);
